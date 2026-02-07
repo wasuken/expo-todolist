dac27d7e33bc3c38530fc973fdb51375dd59f834
wasu <wasuken1@gmail.com>
2026-02-07 18:13:13 +0900

    追加eslint

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['plugin:react/recommended', 'plugin:@typescript-eslint/recommended'],
+  rules: {
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+    '@typescript-eslint/no-unused-vars': 'error',
+  },
+};
